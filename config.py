8ac3a99b03b8e5dc08f8d82b3cc0d8a5bb40eb25
Elsa Schieffelin <ems18@rice.edu>
2019-03-21 20:30:21 -0500

    loop and dict creation

--- config.py
+++ config.py
@@ -1,1 +1,5 @@
-api_key = "-6h7g0eU0bZhAiK7GquXvfaR2no3cgHbYHeR7u3Bt5bg1UQBVl-gYVrjOA5k3TPD"
+genius_api_key = "-6h7g0eU0bZhAiK7GquXvfaR2no3cgHbYHeR7u3Bt5bg1UQBVl-gYVrjOA5k3TPD"
+
+spotify_elsa_id = "13764705a49f421bb41a6c08dfe02566"
+spotify_elsa_secret = "8c270774da8a4636a5165a98749ecd2f"
+
